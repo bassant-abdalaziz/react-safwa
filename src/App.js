9e9d423c35f13504720e1d861cb20668cb85e934
Bassant <bassantabdalaziz1999@gmail.com>
2023-08-21 01:56:14 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './component/Home'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-        <Home/>
-      </BrowserRouter>
+      <Home/>
     </div>
   );
 }
