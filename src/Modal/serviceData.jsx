b572079553701c2d93b61fe4a0c9cdfe1c2c2335
Bassant <bassantabdalaziz1999@gmail.com>
2023-08-31 11:33:51 +0300

    services

--- src/Modal/serviceData.jsx
+++ src/Modal/serviceData.jsx
@@ -1,27 +1,28 @@
 
-import img1 from "../assets/img-3.png"
-import img2 from "../assets/img-4.png"
-import img3 from "../assets/img-2.png"
+import img1 from "../assets/img-4.png"
+import img2 from "../assets/quran.jpg"
+import img3 from "../assets/img-3.png"
 
 const serviceData=[
     {
         "id" : 1,
         "alt":"img1",
         "img": img1,
-        "title": "معلمين و معلمات متخصصين حضوري و اونلاين فى جميع المواد الدراسية"
+        "title": "خبرة طويلة في المناهج السعودية اهلي حكومي إنترناشيونال"
     },
     {
         "id" : 2,
         "alt":"img2",
         "img": img2,
-        "title": "خبرة طويلة في المناهج السعودية اهلي حكومي إنترناشيونال"
+        "title": "نوفر محفظات مجازات بأجر رمزى"
     },
     {
         "id" : 3,
         "alt":"img3",
         "img": img3,
-        "title": "برامج تحفيظ مصممة خصيصًا لجميع الأعمار ومستويات التحفيظ"
+        "title": "معلمين و معلمات متخصصين حضوري و اونلاين فى جميع المواد الدراسية"
     }
+
 ]
 
 export default serviceData;
